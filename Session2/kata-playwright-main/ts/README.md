92a05687abddc2acd7ac47b79fb6660ce683577d
SEBASTIAN <spestanam273@gmail.com>
2025-01-29 09:37:28 +0100

    add ts project

---
+++ Session2/kata-playwright-main/ts/README.md
@@ -0,0 +1,11 @@
+# Running tests
+
+First, fix the URL in `playwright.config.ts`.
+
+Then run:
+
+```
+$ npm install
+$ npx playwright install chromium
+$ npx playwright test --headed
+```
---
+++ Session2/kata-playwright-main/ts/playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from '@playwright/test'
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: false,
+  use: {
+    // TODO: fix the URL
+    baseURL: 'https://f.se2.hr.dmerej.info',
+
+    trace: 'on-first-retry'
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], headless: false }
+    }
+  ]
+
+})
